4bf9dc89ae049413a4f59756a84b7211e627b6e7
Brian Mallen <bmallen89@gmail.com>
2020-04-14 12:25:45 -0400

    update pages

--- js/script.js
+++ js/script.js
@@ -22,9 +22,13 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
 if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-  mybutton.style.display = "block";
+  if (mybutton) {
+   mybutton.style.display = "block";
+  }
 } else {
+  if (mybutton) {
   mybutton.style.display = "none";
+  }
 }
 }
 
